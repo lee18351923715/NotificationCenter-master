93b96031a169f8c73b304a2ee8afaba121ec260a
lee18351923715 <18351923715>
2019-08-07 16:57:05 +0800

    取消全选失败

--- app/src/main/java/com/sample/notificationcenter/MainActivity.java
+++ app/src/main/java/com/sample/notificationcenter/MainActivity.java
@@ -512,6 +512,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             recyclerView.scrollToPosition(0);
         }
     }
+//    /**
+//     * 手动添加数据
+//     */
+//    private void setData() {
+//        Uri newUri;
+//        /**
+//         * 点击刷新随机发送一条信息
+//         */
+//        int choice = (int)(Math.random()*10+1);
+//        switch (choice){
+//            case 1:
+//                ContentValues values1 = Utils.set("行程评分",
+//                        "本次行驶距离：xx公里；油耗：xx;急加速：xx次；急减速：xx次，急转弯：xx次。建议减速慢行，平稳行驶，可减少油耗，降低安全风险，祝您用车愉快！",
+//                        0,2);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values1);
+//                break;
+//            case 2:
+//                ContentValues values2 = Utils.set("车辆保养提醒",
+//                        "尊敬的用户，累计行驶公里数，达到保养里程，请联系上汽大通官方4S店预约保养，谢谢。",0,3);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values2);
+//                break;
+//            case 3:
+//                ContentValues values3 = Utils.set("保养预约到期提醒",
+//                        "尊敬的用户，您的爱车，在年月日时间有一次维保服务预约。预约门店地址：xxx，联系电话xxx。",0,4 );
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values3);
+//                break;
+//            case 4:
+//                ContentValues values4 = Utils.set("车检提醒","您的【车辆昵称】还有xx天要进行车检，请于x年x月x日前去完成车检，谢谢。",0,6);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values4);
+//                break;
+//            case 5:
+//                ContentValues values5 = Utils.set("目的地推送","您收到来自xxx发送的目的地：上海市杨浦区军工路2500号。",0,5);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values5);
+//                break;
+//            case 6:
+//                ContentValues values6 = Utils.set("行程提醒","15分钟后开车去公司。",0,5);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values6);
+//                break;
+//            case 7:
+//                ContentValues values7 = Utils.set("低油量提醒","前油量偏低，点击前往附近加油站加油，保证车辆正常行驶",0,5);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values7);
+//                break;
+//            case 8:
+//                ContentValues values8 = Utils.set("可续里程不足","您的车辆可续里程不足以到达目的地，请前往最近加油站加油。",
+//                        0,5);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values8);
+//                break;
+//            case 9:
+//                ContentValues values9 = Utils.set("天气提醒","明天有雨，请记得带伞。",0,2);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values9);
+//                break;
+//            case 10:
+//                ContentValues values10 = Utils.set("促销活动","运营商提供的活动消息体", 0, 4);
+//                newUri = getContentResolver().insert(MetaData.TableMetaData.CONTENT_URI,values10);
+//                break;
+//        }
+//
+//        if (adapter != null) {
+//
+//            edit.setEnabled(true);
+//
+////            int lastPosition = list.size() - 1;
+////            //刷新列表数据
+////            adapter.notifyItemInserted(lastPosition);
+//            list = getNews();
+//            adapter = new MessageAdapter(this,list);
+//            recyclerView.setAdapter(adapter);
+//            /**
+//             * 如果不需要动画效果
+//             * 就删掉 adapter.notifyItemInserted(lastPosition);
+//             * 用 adapter.notifyDataSetChanged();
+//             */
+//            //recyclerView.smoothScrollToPosition(lastPosition);
+//        }
+//    }
     /**
      * 从数据库中初始化模拟新闻数据
      * 将数据库的信息按倒序方式输出
